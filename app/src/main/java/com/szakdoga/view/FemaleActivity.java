cd45276a81f01b4dcfa3d83580fe9b0523bbb8f6
norbertHarmati <norbert.harmati92@gmail.com>
2021-01-18 20:36:07 +0100

    Instantiate femaleBackButton and set its clickListener to step back GenderA…

--- app/src/main/java/com/szakdoga/view/FemaleActivity.java
+++ app/src/main/java/com/szakdoga/view/FemaleActivity.java
@@ -1,6 +1,9 @@
 package com.szakdoga.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.szakdoga.R;
 
@@ -9,5 +12,14 @@ public class FemaleActivity extends GenderActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.female_window);
+
+        Button femaleBack = findViewById(R.id.femaleBackButton);
+        femaleBack.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(view.getContext(), GenderActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
