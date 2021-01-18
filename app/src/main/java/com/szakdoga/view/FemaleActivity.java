bbab090e0d62bf92a8508174baf2d98cbcea2246
norbertHarmati <norbert.harmati92@gmail.com>
2021-01-18 20:08:35 +0100

    Create FemaleActivity class

---
+++ app/src/main/java/com/szakdoga/view/FemaleActivity.java
@@ -0,0 +1,13 @@
+package com.szakdoga.view;
+
+import android.os.Bundle;
+
+import com.szakdoga.R;
+
+public class FemaleActivity extends GenderActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.female_window);
+    }
+}
