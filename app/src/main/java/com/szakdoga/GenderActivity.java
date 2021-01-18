64f94d60256cad07f55d0e29ca808e8b59300e3e
norbertHarmati <norbert.harmati92@gmail.com>
2021-01-18 16:03:29 +0100

    Create GenderActivity class

---
+++ app/src/main/java/com/szakdoga/GenderActivity.java
@@ -0,0 +1,12 @@
+package com.szakdoga;
+
+import android.os.Bundle;
+
+public class GenderActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gender_window);
+    }
+}
