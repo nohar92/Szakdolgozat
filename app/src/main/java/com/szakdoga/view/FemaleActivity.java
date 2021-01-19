d1d379085118857ecf941765e1e9f64c0da36efd
norbertHarmati <norbert.harmati92@gmail.com>
2021-01-19 22:10:11 +0100

    Create ArrayAdapter for Spinner

--- app/src/main/java/com/szakdoga/view/FemaleActivity.java
+++ app/src/main/java/com/szakdoga/view/FemaleActivity.java
@@ -3,16 +3,20 @@ package com.szakdoga.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.szakdoga.R;
 
 public class FemaleActivity extends GenderActivity {
+    //implements AdapterView.OnItemSelectedListener {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.female_window);
 
+        // Create back button and its clickListener to step back GenderActivity
         Button femaleBack = findViewById(R.id.femaleBackButton);
         femaleBack.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
@@ -21,5 +25,29 @@ public class FemaleActivity extends GenderActivity {
 
             }
         });
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        // Specify the layout to use when the list of choices appears
+        // Apply the adapter to the spinner
+/*
+        Spinner spinner = (Spinner) findViewById(R.id.womanActivitySpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.activity_levels, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        //   spinner.setOnItemSelectedListener(this);
+*/
     }
+/*
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+    */
 }
