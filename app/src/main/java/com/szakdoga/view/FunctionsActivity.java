4d91ea01d6521e606ceaa15761c1045fffc905b2
norbertHarmati <norbert.harmati92@gmail.com>
2021-01-20 19:12:03 +0100

    Create FunctionsActivity

---
+++ app/src/main/java/com/szakdoga/view/FunctionsActivity.java
@@ -0,0 +1,15 @@
+package com.szakdoga.view;
+
+import android.os.Bundle;
+
+import com.szakdoga.R;
+
+public class FunctionsActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.functionalities_window);
+}
+
+}
