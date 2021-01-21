6b37b626b2b64f11f2a95df36bab359732cdd737
norbertHarmati <norbert.harmati92@gmail.com>
2021-01-21 15:16:22 +0100

    Create MenuOfferActivity and menu_offer_window

---
+++ app/src/main/java/com/szakdoga/view/MenuOfferActivity.java
@@ -0,0 +1,14 @@
+package com.szakdoga.view;
+
+import android.os.Bundle;
+
+import com.szakdoga.R;
+
+public class MenuOfferActivity extends FunctionsActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_offer_window);
+    }
+}
