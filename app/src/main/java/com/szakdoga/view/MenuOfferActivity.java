aa2ac3b86eb88c73ef0d2953ed9044ce6e24ff60
norbertHarmati <norbert.harmati92@gmail.com>
2021-01-21 15:55:42 +0100

    Create back button and set its clickListener

--- app/src/main/java/com/szakdoga/view/MenuOfferActivity.java
+++ app/src/main/java/com/szakdoga/view/MenuOfferActivity.java
@@ -1,6 +1,9 @@
 package com.szakdoga.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.szakdoga.R;
 
@@ -10,5 +13,15 @@ public class MenuOfferActivity extends FunctionsActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_offer_window);
+
+        // Create back button and its clickListener to step back FunctionsActivity
+        Button back = findViewById(R.id.backButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), FunctionsActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
