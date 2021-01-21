5b1ad304566702115e5b7d3d8a9c22f6397f6512
norbertHarmati <norbert.harmati92@gmail.com>
2021-01-21 16:49:41 +0100

    Create clickable texts(Bulking, Cutting, Keep) to step forward MenuOfferActivity

--- app/src/main/java/com/szakdoga/view/CalculationsActivity.java
+++ app/src/main/java/com/szakdoga/view/CalculationsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.szakdoga.R;
 
@@ -21,6 +22,36 @@ public class CalculationsActivity extends FunctionsActivity{
                 startActivity(intent);
 
             }
+        });
+
+        // Create bulking text and set its clickListener to step forward MenuOfferActivity
+        TextView bulking = findViewById(R.id.bulking);
+        bulking.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                Intent intent = new Intent(view.getContext(),MenuOfferActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        // Create cutting text and set its clickListener to step forward MenuOfferActivity
+        TextView cutting = findViewById(R.id.cutting);
+        cutting.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                Intent intent = new Intent(view.getContext(),MenuOfferActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        // Create keep text clickable and set its clickListener to step forward MenuOfferActivity
+        TextView keep = findViewById(R.id.keep);
+        keep.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                Intent intent = new Intent(view.getContext(),MenuOfferActivity.class);
+                startActivity(intent);
+            }
+
         });
     }
 }
