1f600ccf8e9d7e8ebc9ab7ccc5cddda5e75178e8
norbertHarmati <norbert.harmati92@gmail.com>
2021-02-06 19:05:26 +0100

    Update AlertDialog and insertData methods

--- app/src/main/java/com/szakdoga/view/DetailsActivity.java
+++ app/src/main/java/com/szakdoga/view/DetailsActivity.java
@@ -1,7 +1,6 @@
 package com.szakdoga.view;
 
 import android.app.AlertDialog;
-import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,22 +10,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.szakdoga.R;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class DetailsActivity extends MainActivity {
-    //implements AdapterView.OnItemSelectedListener {
 
-    EditText txtName, txtGender, txtAge,txtWeight,txtHeight,txtNeck,txtWaist,txtHip,txtActivityLv;
+    EditText txtName, txtGender, txtAge, txtWeight, txtHeight, txtNeck, txtWaist, txtHip, txtActivityLv;
     Button saveAndForward;
 
     public void onCreate(Bundle savedInstanceState) {
@@ -47,7 +42,7 @@ public class DetailsActivity extends MainActivity {
         saveAndForward = findViewById(R.id.save_button);
         saveAndForward.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-/*
+
                 String name = txtName.getText().toString().trim();
                 String gender = txtGender.getText().toString().trim();
                 String age = txtAge.getText().toString().trim();
@@ -61,63 +56,35 @@ public class DetailsActivity extends MainActivity {
 
                 if (TextUtils.isEmpty(name)) {
                     alertDialog();
-                } else if(TextUtils.isEmpty(gender)) {
+                } else if (TextUtils.isEmpty(gender)) {
                     alertDialog();
-                } else if(TextUtils.isEmpty(age)) {
+                } else if (TextUtils.isEmpty(age)) {
                     alertDialog();
-                } else if(TextUtils.isEmpty(weight)) {
+                } else if (TextUtils.isEmpty(weight)) {
                     alertDialog();
-                } else if(TextUtils.isEmpty(height)) {
+                } else if (TextUtils.isEmpty(height)) {
                     alertDialog();
-                } else if(TextUtils.isEmpty(neck)) {
+                } else if (TextUtils.isEmpty(neck)) {
                     alertDialog();
-                } else if(TextUtils.isEmpty(waist)) {
+                } else if (TextUtils.isEmpty(waist)) {
                     alertDialog();
-                } else if(TextUtils.isEmpty(hip)) {
+                } else if (TextUtils.isEmpty(hip)) {
                     alertDialog();
-                } else if(TextUtils.isEmpty(activityLv)) {
+                } else if (TextUtils.isEmpty(activityLv)) {
                     alertDialog();
                 } else {
                     insertData();
                     Intent intent = new Intent(view.getContext(), FunctionsActivity.class);
                     startActivity(intent);
                 }
-*/
-                Intent intent = new Intent(view.getContext(), FunctionsActivity.class);
-                startActivity(intent);
             }
         });
-        /*
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        // Specify the layout to use when the list of choices appears
-        // Apply the adapter to the spinner
-
-        Spinner activitySpinner = (Spinner) findViewById(R.id.activitySpinner);
-        ArrayAdapter<CharSequence> activityAdapter = ArrayAdapter.createFromResource(
-                this, R.array.activity_levels, android.R.layout.simple_spinner_item);
-        activityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        activitySpinner.setAdapter(activityAdapter);
-        //   spinner.setOnItemSelectedListener(this);
-        */
-
-        /*
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // An item was selected. You can retrieve the selected item using
-        // parent.getItemAtPosition(pos)
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-    */
     }
 
     private void alertDialog() {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setMessage("Töltse ki a mezőt/mezőket!");
-        dialog.setTitle("Tip");
+        dialog.setMessage("Töltse ki az üresen hagyott mezőt/mezőket!");
+        dialog.setTitle("Hiányzó adatok");
         dialog.setPositiveButton("OK",
                 new DialogInterface.OnClickListener() {
                     @Override
@@ -130,6 +97,7 @@ public class DetailsActivity extends MainActivity {
     }
 
     public void insertData() {
+
         String name = txtName.getText().toString().trim();
         String gender = txtGender.getText().toString().trim();
         String age = txtAge.getText().toString().trim();
@@ -140,65 +108,44 @@ public class DetailsActivity extends MainActivity {
         String hip = txtHip.getText().toString().trim();
         String activityLv = txtActivityLv.getText().toString().trim();
 
-
-        if (name.isEmpty()) {
-            Toast.makeText(this, "Enter your name", Toast.LENGTH_LONG).show();
-        } else if (gender.isEmpty()) {
-            Toast.makeText(this, "Enter your gender", Toast.LENGTH_LONG).show();
-        } else if (age.isEmpty()) {
-            Toast.makeText(this, "Enter your age", Toast.LENGTH_LONG).show();
-        } else if (weight.isEmpty()) {
-            Toast.makeText(this, "Enter your weight", Toast.LENGTH_LONG).show();
-        } else if (height.isEmpty()) {
-            Toast.makeText(this, "Enter your height", Toast.LENGTH_LONG).show();
-        } else if (neck.isEmpty()) {
-            Toast.makeText(this, "Enter your neck circumference", Toast.LENGTH_LONG).show();
-        } else if (waist.isEmpty()) {
-            Toast.makeText(this, "Enter your waist circumference", Toast.LENGTH_LONG).show();
-        } else if (hip.isEmpty()) {
-            Toast.makeText(this, "Enter your hip circumference", Toast.LENGTH_LONG).show();
-        } else if (activityLv.isEmpty()) {
-            Toast.makeText(this, "Enter your activity level", Toast.LENGTH_LONG).show();
-        } else {
-
-
-            StringRequest request = new StringRequest(Request.Method.POST,"https://uncloudy-refurbishm.000webhostapp.com/insert.php",
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            if (response.equalsIgnoreCase("Data Inserted")) {
-                                Toast.makeText(DetailsActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
-                            } else {
-                                Toast.makeText(DetailsActivity.this, response, Toast.LENGTH_LONG).show();
-                            }
+        StringRequest request = new StringRequest(Request.Method.POST, "https://uncloudy-refurbishm.000webhostapp.com/insert.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.equalsIgnoreCase("Data Inserted")) {
+                            Toast.makeText(DetailsActivity.this, "Adatok feltöltve", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(DetailsActivity.this, response, Toast.LENGTH_LONG).show();
                         }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(DetailsActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
-                }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(DetailsActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
             }
-            ) {
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-
-                    Map<String, String> params = new HashMap<String, String>();
-                    params.put("username", name);
-                    params.put("gender", gender);
-                    params.put("age", age);
-                    params.put("weight", weight);
-                    params.put("height", height);
-                    params.put("neck", neck);
-                    params.put("waist", waist);
-                    params.put("hip", hip);
-                    params.put("activity_level",activityLv);
-
-                    return params;
-                }
-            };
-
-            RequestQueue requestQueue = Volley.newRequestQueue(DetailsActivity.this);
-            requestQueue.add(request);
         }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("username", name);
+                params.put("gender", gender);
+                params.put("age", age);
+                params.put("weight", weight);
+                params.put("height", height);
+                params.put("neck", neck);
+                params.put("waist", waist);
+                params.put("hip", hip);
+                params.put("activity_level", activityLv);
+
+                return params;
+            }
+        };
+
+        MySingleton.getInstance(this).addToRequestQueue(request);
+
     }
+
 }
+
