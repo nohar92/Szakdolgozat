1a5505a8fa8147ac6033ace243ce5bdc9730efc7
norbertHarmati <norbert.harmati92@gmail.com>
2021-02-12 01:59:34 +0100

    Create User class

---
+++ app/src/main/java/com/szakdoga/model/User.java
@@ -0,0 +1,102 @@
+package com.szakdoga.model;
+
+public class User {
+
+
+    public User() {}
+
+    private int id;
+    private String name;
+    private String gender;
+    private int age;
+    private double weight;
+    private int height;
+    private double neck;
+    private double waist;
+    private double hip;
+    private int activityLv;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public double getNeck() {
+        return neck;
+    }
+
+    public void setNeck(double neck) {
+        this.neck = neck;
+    }
+
+    public double getWaist() {
+        return waist;
+    }
+
+    public void setWaist(double waist) {
+        this.waist = waist;
+    }
+
+    public double getHip() {
+        return hip;
+    }
+
+    public void setHip(double hip) {
+        this.hip = hip;
+    }
+
+    public int getActivityLv() {
+        return activityLv;
+    }
+
+    public void setActivityLv(int activityLv) {
+        this.activityLv = activityLv;
+    }
+
+
+}
