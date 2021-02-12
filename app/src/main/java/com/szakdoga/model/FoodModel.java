43357f9dab7c84566f6b9438589a67d658816dc5
norbertHarmati <norbert.harmati92@gmail.com>
2021-02-12 22:43:27 +0100

    Create FoodModel and implement foodAmount methods

--- app/src/main/java/com/szakdoga/model/FoodModel.java
+++ app/src/main/java/com/szakdoga/model/FoodModel.java
@@ -5,24 +5,72 @@ public class FoodModel {
     FoodMacro foodMacro;
     CalculationModel calcModel;
 
-    public FoodModel(FoodMacro foodMacro){
+    public FoodModel(FoodMacro foodMacro,CalculationModel calcModel){
         this.foodMacro = foodMacro;
+        this.calcModel = calcModel;
 
     }
 
-    public int getBreakfastAmount(){
-        int maxCalorie = (int) (calcModel.totalEnergy() * 0.35);
-        return (maxCalorie/ foodMacro.getCalorie())*100;
+
+
+    public int getBulkingBreakfastAmount(){
+        double maxCalorie =  calcModel.bulking() * 0.35;
+        return (int) ((maxCalorie/ foodMacro.getKcal())*100);
     }
 
-    public int getLunchAmount(){
-        int maxCalorie = (int) (calcModel.totalEnergy() *0.45);
-        return (maxCalorie/ foodMacro.getCalorie())*100;
+    public int getBulkingLunchAmount(){
+        double maxCalorie =  (calcModel.bulking() *0.45);
+        return (int) (maxCalorie/ foodMacro.getKcal())*100;
     }
 
-    public int getDinnerAmount(){
-        int maxCalorie = (int) (calcModel.totalEnergy() *0.3);
-        return (maxCalorie/ foodMacro.getCalorie())*100;
+    public int getBulkingDinnerAmount(){
+        double maxCalorie =  (calcModel.bulking() *0.3);
+        return (int) (maxCalorie/ foodMacro.getKcal())*100;
     }
+
+    public int getCuttingBreakfastAmount(){
+        double maxCalorie =  calcModel.cutting() * 0.35;
+        return (int) ((maxCalorie/ foodMacro.getKcal())*100);
+    }
+
+    public int getCuttingLunchAmount(){
+        double maxCalorie =  (calcModel.cutting() *0.45);
+        return (int) (maxCalorie/ foodMacro.getKcal())*100;
+    }
+
+    public int getCuttingDinnerAmount(){
+        double maxCalorie =  (calcModel.cutting() *0.3);
+        return (int) (maxCalorie/ foodMacro.getKcal())*100;
+    }
+
+    public int getMaintanBreakfastAmount(){
+        double maxCalorie =  calcModel.maintenance() * 0.35;
+        return (int) ((maxCalorie/ foodMacro.getKcal())*100);
+    }
+
+    public int getMaintanLunchAmount(){
+        double maxCalorie =  (calcModel.maintenance() *0.45);
+        return (int) (maxCalorie/ foodMacro.getKcal())*100;
+    }
+
+    public int getMaintanDinnerAmount(){
+        double maxCalorie =  (calcModel.maintenance() *0.3);
+        return (int) (maxCalorie/ foodMacro.getKcal())*100;
+    }
+
+/*public double getMethodResponse(String method){
+
+        switch (method){
+            case "bulking":
+                return calcModel.bulking();
+                break;
+            case "cutting":
+                return calcModel.cutting();
+            case "cutting":
+                return calcModel.cutting();
+        }
+
+}*/
+
 
 }
