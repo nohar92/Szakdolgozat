d90f9a516555917462731676ee250bf68861702a
norbertHarmati <norbert.harmati92@gmail.com>
2021-02-22 20:56:18 +0100

    Create RecipeActivity and its xml file

---
+++ app/src/main/java/com/szakdoga/view/recipes/RecipeActivity2.java
@@ -0,0 +1,15 @@
+package com.szakdoga.view.recipes;
+
+import android.os.Bundle;
+
+import com.szakdoga.R;
+import com.szakdoga.view.BulkingMenuOfferActivity;
+
+public class RecipeActivity2 extends BulkingMenuOfferActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recipe_window2);
+    }
+}
